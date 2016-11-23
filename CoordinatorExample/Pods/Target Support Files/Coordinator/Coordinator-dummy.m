f42f26661d2a2fa2a651be8019ab41614d555788
Aleksandar Vacic <av@radianttap.com>
2016-11-23 15:06:55 +0100

    Switched to proper podspec development structure

---
+++ CoordinatorExample/Pods/Target Support Files/Coordinator/Coordinator-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Coordinator : NSObject
+@end
+@implementation PodsDummy_Coordinator
+@end
---
+++ CoordinatorExample/Pods/Target Support Files/Coordinator/Coordinator-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double CoordinatorVersionNumber;
+FOUNDATION_EXPORT const unsigned char CoordinatorVersionString[];
+
