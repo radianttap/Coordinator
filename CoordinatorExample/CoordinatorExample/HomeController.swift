236f8a8d86c5a60ee34846eca1449a60f732cfd6
Aleksandar Vacic <av@radianttap.com>
2017-05-14 11:24:43 +0200

    Building out UI part

---
+++ CoordinatorExample/CoordinatorExample/HomeController.swift
@@ -0,0 +1,13 @@
+//
+//  HomeController.swift
+//  CoordinatorExample
+//
+//  Created by Aleksandar Vacić on 14.5.17..
+//  Copyright © 2017. Radiant Tap. All rights reserved.
+//
+
+import UIKit
+
+final class HomeController: UIViewController, StoryboardLoadable {
+
+}
