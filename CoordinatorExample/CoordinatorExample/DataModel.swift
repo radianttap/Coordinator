21e2c33fc5cea2b7930202609d14d0f48e82f295
Aleksandar Vacic <av@radianttap.com>
2017-05-14 09:04:47 +0200

    Clean stuff from simpler demo

--- CoordinatorExample/CoordinatorExample/DataModel.swift
+++ CoordinatorExample/CoordinatorExample/DataModel.swift
@@ -8,43 +8,17 @@
 
 import Foundation
 
-//	pure Swift class
-class Person {
-	let name: String
-	init(name: String) {
-		self.name = name
-	}
-}
-
-struct Place {
-	let name: String
-}
-
-enum Direction {
-	case south
-	case north
-}
-
-//	Objective-C subclass
-class PersonObjc: NSObject {
-	let name: String
-	init(name: String) {
-		self.name = name
-	}
-}
-
-
 //	SOLUTION:
 //	Boxing pure Swift type into Objective-C friendly form
 
-class PersonBox: NSObject {
-	let unbox: Person
-	init(_ value: Person) {
-		self.unbox = value
-	}
-}
-
-extension Person {
-	var boxed: PersonBox { return PersonBox(self) }
-}
-
+//class PersonBox: NSObject {
+//	let unbox: Person
+//	init(_ value: Person) {
+//		self.unbox = value
+//	}
+//}
+//
+//extension Person {
+//	var boxed: PersonBox { return PersonBox(self) }
+//}
+//
