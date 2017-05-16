89db0fb1c6bd093ba10205f046e1fe0e7056603f
Aleksandar Vacic <av@radianttap.com>
2017-05-16 08:37:08 +0200

    Added some color specs

--- CoordinatorExample/CoordinatorExample/DataModel/Color.swift
+++ CoordinatorExample/CoordinatorExample/DataModel/Color.swift
@@ -14,7 +14,25 @@ struct Color {
 }
 
 extension Color {
+	static let c11: Color = {
+		return Color(name: "Off-White", code: 11)
+	}()
 	static let c18: Color = {
-		return Color(name: "Black", code: 18)
+		return Color(name: "Anthracite", code: 18)
+	}()
+	static let c25: Color = {
+		return Color(name: "Beige", code: 25)
+	}()
+	static let c29: Color = {
+		return Color(name: "Brown", code: 29)
+	}()
+	static let c39: Color = {
+		return Color(name: "Navy", code: 39)
+	}()
+	static let c40: Color = {
+		return Color(name: "Powder", code: 40)
+	}()
+	static let c69: Color = {
+		return Color(name: "Forest", code: 69)
 	}()
 }
