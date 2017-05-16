9565b36d06eb15c29a8e9cd28544e38eba43ed60
Aleksandar Vacic <av@radianttap.com>
2017-05-16 22:09:19 +0200

    Wrapper to avoid string literal all over the place

---
+++ CoordinatorExample/CoordinatorExample/Storyboards.swift
@@ -0,0 +1,15 @@
+//
+//  Storyboards.swift
+//  CoordinatorExample
+//
+//  Created by Aleksandar Vacić on 16.5.17..
+//  Copyright © 2017. Radiant Tap. All rights reserved.
+//
+
+import UIKit
+
+extension UIStoryboard {
+	static var app: UIStoryboard = {
+		return UIStoryboard(name: "App", bundle: nil)
+	}()
+}
