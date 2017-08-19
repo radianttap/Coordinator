849cfdeece35f458aeada4005b335b7d69df7ad8
Aleksandar Vacic <av@radianttap.com>
2017-08-20 00:17:32 +0200

    Re-organized the files into proper folders. Finished AppDependency re-factor
    Added AccountManager too

---
+++ CoordinatorExample/CoordinatorExample/Account/AccountManager.swift
@@ -0,0 +1,14 @@
+//
+//  AccountManager.swift
+//  CoordinatorExample
+//
+//  Created by Aleksandar Vacić on 20.8.17..
+//  Copyright © 2017. Radiant Tap. All rights reserved.
+//
+
+import Foundation
+
+
+final class AccountManager {
+	
+}
