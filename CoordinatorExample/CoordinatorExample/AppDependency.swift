89ada7237be828956d1777e2f6ac114382f17103
Aleksandar Vacic <av@radianttap.com>
2017-09-10 18:12:36 +0200

    Moved protocol into its own file

--- CoordinatorExample/CoordinatorExample/AppDependency.swift
+++ CoordinatorExample/CoordinatorExample/AppDependency.swift
@@ -64,25 +64,3 @@ extension AppDependency {
 	var boxed: AppDependencyBox { return AppDependencyBox(self) }
 }
 
-
-
-
-//	Protocol you need to apply to all the Coordinators,
-//	so the new `dependencies` value is propagated down
-
-protocol NeedsDependency: class {
-	var dependencies: AppDependency? { get set }
-}
-
-extension NeedsDependency where Self: Coordinating {
-	func updateChildCoordinatorDependencies() {
-		self.childCoordinators.forEach { (_, coordinator) in
-			if let c = coordinator as? NeedsDependency {
-				c.dependencies = dependencies
-			}
-		}
-	}
-}
-
-
-
