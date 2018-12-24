e0a2d65280bb7254d7a76e84ac3028ec1e526d88
Aleksandar Vacic <av@codeaplus.com>
2018-12-24 09:29:13 +0100

    Improved the documentation

--- Coordinator/Coordinating.swift
+++ Coordinator/Coordinating.swift
@@ -8,48 +8,52 @@
 
 import UIKit
 
-///	Helper protocol to simplify coordinator‘s hierarchy walk-up.
+///	Protocol to define what is required for an object to be Coordinator.
 ///
-///	Note: This protocol should not be subclassed.
+///	It also simplifies coordinator hierarchy management.
 public protocol Coordinating: class {
-	///	Unique string to identify specific Coordinator instance
+	///	Unique string to identify specific Coordinator instance.
 	///
 	///	By default it will be String representation of the Coordinator's subclass.
-	///	If you directly instantiate `Coordinator<T>`, then you need to set it manually
+	///	If you directly instantiate `Coordinator<T>`, then you need to set it manually.
 	var identifier: String { get }
 
-	/// Parent Coordinator can be any other Coordinator
+	/// Parent Coordinator can be any other Coordinator.
 	var parent: Coordinating? { get set }
 
-	///	A dictionary of child Coordinators, where key is Coordinator's identifier property
+	///	A dictionary of child Coordinators, where key is Coordinator's identifier property.
 	var childCoordinators: [String: Coordinating] { get }
 
 	///	Returns either `parent` coordinator or `nil` if there isn‘t one
 	var coordinatingResponder: UIResponder? { get }
 
 	///	Tells the coordinator to start, which means at the end of this method it should
-	///	display some UIViewController
+	///	display some UIViewController.
 	func start(with completion: @escaping () -> Void)
 
 	///	Tells the coordinator to stop, which means it should clear out any internal stuff
-	///	it possible tracks. I.e. list of shown UIViewControllers
+	///	it possibly tracks.
+	///	I.e. list of shown `UIViewController`s.
 	func stop(with completion: @escaping () -> Void)
 
-	///	Call this method if the Coordinator is using UINavigationController
-	///	as rootViewController and customer has just pop-ed out of `coordinator` domain
+	///	Essentially, this means that Coordinator requests from its parent to stop it.
 	///
-	///	This allows you to inform `coordinator.parent` it should activate something else
-	///	(like previous childCoordinator, if there is one)
+	///	Useful in cases where a particular Coordinator instance know that at particular
+	///	moment none of its UIVCs will be visible or useful anymore.
+	///	This is a chance for parentCoordinator to nicely transitions to some other Coordinator.
 	func coordinatorDidFinish(_ coordinator: Coordinating, completion: @escaping () -> Void)
 
-	///	Adds the supplied coordinator into its childCoordinators dictionary and calls its `start` method
+	///	Adds the supplied coordinator into its `childCoordinators` dictionary and calls its `start` method
 	func startChild(coordinator: Coordinating, completion: @escaping () -> Void)
 
-	///	Calls `stop` on the supplied coordinator and removes it from its childCoordinators dictionary
+	///	Calls `stop` on the supplied coordinator and removes it from its `childCoordinators` dictionary
 	func stopChild(coordinator: Coordinating, completion: @escaping () -> Void)
 
-	///	Coordinator will assign itself as parentCoordinator of its rootController,
-	///	ready to start displaying its content View Controllers.
+	///	Activate Coordinator which was used before.
+	///
+	///	At the least, this Coordinator should assign itself as `parentCoordinator` of its `rootViewController`,
+	///	ready to start displaying its content View Controllers. This is required due to the possibility that
+	///	multiple Coordinator can share one UIViewController as their root VC.
 	///
 	///	See NavigationCoordinator for one possible usage.
 	func activate()
