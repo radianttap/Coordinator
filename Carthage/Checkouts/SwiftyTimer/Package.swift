ae0b278ee75baa5289bd7de482e25cfc3bcf6166
Aleksandar Vacić <av@radianttap.com>
2019-04-05 21:25:33 +0200

    Removed all traces of the example app

--- Carthage/Checkouts/SwiftyTimer/Package.swift
+++
@@ -1,8 +0,0 @@
-import PackageDescription
-
-let package = Package(
-	name: "SwiftyTimer",
-	dependencies: [],
-    exclude: ["Sources/Info.plist", "Sources/SwiftyTimer.h", "SwiftyTimerTests"]
-    
-)
--- Carthage/Checkouts/SwiftyTimer/Sources/SwiftyTimer.h
+++
@@ -1,4 +0,0 @@
-#import <Foundation/Foundation.h>
-
-FOUNDATION_EXPORT double SwiftyTimerVersionNumber;
-FOUNDATION_EXPORT const unsigned char SwiftyTimerVersionString[];
