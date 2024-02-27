bed2e950027608d9bf207e942fe82be0e20dd762
Aleksandar Vacić <av@radianttap.com>
2024-02-27 11:48:28 +0100

    Added visionOS support

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,8 @@ let package = Package(
     name: "Coordinator",
     platforms: [
 		.iOS(.v13),
-		.tvOS(.v13)
+		.tvOS(.v13),
+		.visionOS(.v1)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
